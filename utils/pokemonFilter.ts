42cd6e2fc17defa34611401f7a033231c1ba596d
Lukáš Treml <work@treml.cz>
2022-12-03 20:45:33 +0100

    fix: filter

--- utils/pokemonFilter.ts
+++ utils/pokemonFilter.ts
@@ -2,7 +2,7 @@ import {Pokemon} from "../types/Pokemon";
 
 export const pokemonFilter = (pokemon:Pokemon, searchText?:string) => {
     if (searchText) {
-        return pokemon.name.toLowerCase().includes(searchText)
+        return pokemon.name.toLowerCase().includes(searchText.toLowerCase())
     } else {
         return true
     }
